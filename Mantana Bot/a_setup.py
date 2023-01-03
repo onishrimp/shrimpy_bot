449abd8845f7d75f8aa819ecd0e3a989e820917c
Oni Shrimp <onishrimp@posteo.de>
2023-01-03 20:47:39 +0100

    leaked my token goddamnit

--- Mantana Bot/a_setup.py
+++ Mantana Bot/a_setup.py
@@ -5,5 +5,5 @@ client = commands.Bot(command_prefix="ratio")
 slash = SlashCommand(client=client, sync_commands=True)
 guild_ids = [888711097119559680]
 
-token = "OTMyOTgzODAyODA3MDc4OTgy.Yea64A.yts6C-xeQLQolh1jyyI1D5-IVmw"
+token = "token"
 welcome_channel_name = "welcome"
